d1f099299a419b08e35c0627453095f842a7420c
Roopesh <rrattan.89@gmail.com>
2017-07-12 16:13:40 -0400

    scene 2 setup cast, and sword

--- js/scene2.js
+++ js/scene2.js
@@ -8,29 +8,94 @@ function scene2() {
   stageImg.setAttribute('src', castleImgSrc)
 
   // Setup Cast
+  // Ned Stark
+  var nedContainer = document.createElement('div')
+  nedContainer.id = 'ned-container'
+  stage.append(nedContainer)
+  nedContainer.style.position = 'absolute'
+  nedContainer.style.width = '70%'
+  nedContainer.style.height = '37%'
+  nedContainer.style.top = '55%'
+  nedContainer.style.left = '25%'
+  nedContainer.style.border = 'none'
+  nedContainer.style.background = 'none'
 
-  // Character 1
+  ned = document.querySelector('#ned')
+  nedContainer.append(ned)
+  leather = document.querySelector('#leather-armour')
+  nedContainer.append(leather)
 
-  // Character 2
+  ned.style.border = 'none'
+  leather.style.border = 'none'
+  ned.style.position = 'absolute'
+  ned.style.top = '-125px'
+  ned.style.left = '-30px'
+  leather.style.position = 'absolute'
+  leather.style.bottom = 0
+
+  // Joffrey
+  var joffContainer = document.createElement('div')
+  joffContainer.id = 'joff-container'
+  stage.append(joffContainer)
+  joffContainer.style.position = 'absolute'
+  joffContainer.style.width = '8%'
+  joffContainer.style.height = '31%'
+  joffContainer.style.top = '55%'
+  joffContainer.style.left = '55%'
+
+  joff = document.querySelector('#joffrey')
+  joffContainer.append(joff)
+  joff.style.width = '75%'
+  joff.style.position = 'absolute'
+  joff.style.left = '33%'
+
+  kingWardrobe = document.querySelector('#king')
+  joffContainer.append(kingWardrobe)
+  kingWardrobe.style.position = 'absolute'
+  kingWardrobe.style.bottom = 0
 
   // Setup Props
-
-  // Prop 1
+  // Sword
+  sword = document.querySelector('#sword')
+  stage.append(sword)
+  sword.style.position = 'absolute'
+  sword.style.top = '77%'
+  sword.style.left = '31%'
+  sword.style.height = '5%'
+  sword.style.transform = 'rotateY(150deg)'
 
   // Prop 2
 
   // Action!
 
-  // Stage direction 1
-
+  // Exisiting Start Frame
   frame(function() {
-
+    sword.style.top = '77%'
+    sword.style.left = '31%'
   })
 
-  // Stage direction 2
+  // New Middle Frames
 
   frame(function() {
+    sword.style.top = '74%'
+    sword.style.left = '36%'
+  })
 
+  frame(function() {
+    sword.style.top = '71%'
+    sword.style.left = '41%'
+  })
+
+  frame(function() {
+    sword.style.top = '68%'
+    sword.style.left = '47%'
+  })
+  
+  // Existing End Frame
+
+  frame(function() {
+    sword.style.top = '64%'
+    sword.style.left = '53%'
   })
 
 }
