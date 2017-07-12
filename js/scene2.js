be844c71bcd8ccf9c6a792c5a7616c9555818050
Roopesh <rrattan.89@gmail.com>
2017-07-12 14:51:44 -0400

    scene 2 setup setting

--- js/scene2.js
+++ js/scene2.js
@@ -1,7 +1,12 @@
 function scene2() {
-  console.log('Executing scene 2!')
+
   // Setup Stage
-  stage = document.queryselector('#stage')
+  stage = document.querySelector('#stage')
+  castle = document.querySelector('#castle')
+  stageImg = stage.querySelector('#stage-img')
+  castleImgSrc = castle.getAttribute('src')
+  stageImg.setAttribute('src', castleImgSrc)
+
   // Setup Cast
 
   // Character 1
